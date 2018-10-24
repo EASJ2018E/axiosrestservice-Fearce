febfb466d76156a9a496616d6a7335805f7696e9
Fearce <thekev93@gmail.com>
2018-10-24 12:14:57 +0200

    peters bil tuff

--- src/js/index.ts
+++ src/js/index.ts
@@ -9,4 +9,28 @@ interface ICar {
     price: number;
 }
 
-var uri = "rest-pele-easj-dk.azurewebsites.dk/api/cars";
+var uri = "http://rest-pele-easj-dk.azurewebsites.net/api/cars";
+
+let buttonElement = document.getElementById("getAllButton") as HTMLButtonElement;
+
+buttonElement.addEventListener('click',showAllCars);
+
+function showAllCars():void{
+    axios.get<ICar[]>(uri)
+    .then(function (response:AxiosResponse<ICar[]>):void{
+        let result:string="<ol>";
+        response.data.forEach((car: ICar) =>{
+            result += "<li>"+"Model: " +car.model + " Vendor: "+car.vendor + " Price: " +  car.price.toString() + "</li>";
+        });
+        result += "</ol>";
+
+        document.getElementById("allCars").innerHTML = result;
+
+    })
+    .catch(function(error:AxiosError):void{
+        document.getElementById("allCars").innerHTML = error.message;
+    })
+
+};
+
+
